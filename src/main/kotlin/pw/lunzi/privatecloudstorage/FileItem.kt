f72d57ff481ba8b9f0eab9071bd8062c59dfab9d
LunziQwQ <me@lunzi.space>
2018-04-01 07:07:11 +0800

    Start do something

---
+++ src/main/kotlin/pw/lunzi/privatecloudstorage/FileItem.kt
@@ -0,0 +1,45 @@
+package pw.lunzi.privatecloudstorage
+
+import java.io.File
+import java.io.FileInputStream
+import java.io.FileOutputStream
+import java.net.URL
+import java.nio.file.Path
+import java.nio.file.Paths
+
+/**
+ * ***********************************************
+ * Created by Lunzi on 3/31/18.
+ * Just presonal practice.
+ * Not allowed to copy without permission.
+ * ***********************************************
+ */
+data class FileItem(
+        val ownerName: String,
+        val realPath: Path,
+        val virtualPath: URL,
+        val isUserRootPath: Boolean,
+        val isDictionary: Boolean,
+        var children: List<FileItem>,
+        val isShared:Boolean
+){
+    companion object {
+        val rootPath: Path = Paths.get("/var/cloudStorage/root")
+    }
+}
+
+fun FileItem.isExist() = realPath.toFile().exists()
+
+fun FileItem.save(fis: FileInputStream) {
+    val temp: File = realPath.toFile()
+    if (isDictionary) {
+        if (!isExist()) {
+            temp.mkdirs()
+        }
+    } else {
+        val fos = FileOutputStream(temp)
+        fos.write(fis.readAllBytes())
+    }
+}
+
+fun FileItem.getFile(): FileOutputStream = if (!isDictionary) FileOutputStream(realPath.toFile()) else null!!
